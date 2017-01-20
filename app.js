07004824299690d11035e6f2fb8e324d1352e6e1
naoxiaozhi <zzy_come_on@163.com>
2017-01-20 18:33:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var util=require('./libs/util');
 var wechat_file=path.join(__dirname,'./config/wechat.txt')
 var config={
 	wechat:{
-		appID:'wxa7407da346a4f7c8',
-		appSecret:'fb3c18b50ab33c1e3e64a19bac7c5963',
-		token:'weiying805623',
+		appID:'',
+		appSecret:'',
+		token:'',
 		getAccessToken:function(){
 			return util.readFileAsync(wechat_file)
 		},
